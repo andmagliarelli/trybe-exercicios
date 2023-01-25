2488778d8cec2f99bc785660201758ad52387ae8
andmagliarelli <andmagliarelli@gmail.com>
2023-01-25 11:12:15 -0300

    Atualização exercicios dia 02

--- 01-fundamentos/03-introducao-a-javascript/dia-02-javascript-array-e-for/exercicio-04-lidando-com-arrays.js
+++ 01-fundamentos/03-introducao-a-javascript/dia-02-javascript-array-e-for/exercicio-04-lidando-com-arrays.js
@@ -46,10 +46,8 @@ if (resultado > 20) {
 }
 
 // Utilizando for, descubra qual o maior valor contido no array e imprima-o;
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
 // o valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso atribuíssemos o valor 101 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema, vamos iniciar a variável com o primeiro valor do array. Dessa forma, podemos pular a posição 0 e iniciar com index = 1
-
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let higherNumber = numbers[0];
 
 for (let index = 1; index < numbers.length; index += 1) {
@@ -57,10 +55,12 @@ for (let index = 1; index < numbers.length; index += 1) {
     higherNumber = numbers[index];
   }
 }
-
 console.log(higherNumber);
 
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impares = numbers[0];
+
 
 
 // Utilizando for, descubra qual o menor valor contido no array e imprima-o;
